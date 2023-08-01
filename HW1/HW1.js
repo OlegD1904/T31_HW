bc40b5551cbae7b7d9c13bb524e73165e0f40b34
Dundiev Oleh <dundev1904@gmail.com>
2023-08-01 12:08:12 +0200

    HW1 Исправление ошибок: 1/  пробелы в тексте модального окна 2/добавление [] в console.log 3/доработка кода (if)

--- HW1/HW1.js
+++ HW1/HW1.js
@@ -1,34 +1,46 @@
 "use strict"
-const firstName=prompt(`Введите своё имя:`);
-console.log ([`FirstName`], typeof firstName, firstName);
-const lastName=prompt(`Введите свою фамилию`);
-console.log ([`LastName`],typeof lastName, lastName);
-alert(`Здравствуйте`+""+firstName+""+lastName);
+alert(`Здравствуйте`+" "+firstName+" "+lastName);
 console.log(`##############################################################`)
 const userAge=+prompt(`Введите свой год рождения (xxxx):`);
 const todayYear=+prompt(`Введите год в настоящем времени (xxxx):`);
 const resultAge=todayYear-userAge;
 console.log([`userAge`], typeof resultAge, resultAge);
-alert(`Ваш возрост`+""+ resultAge)
+alert(`Ваш возрост`+` `+ resultAge);
+
 console.log(`##############################################################`);
 if(resultAge<18){
-    console.log(`Вам запрещён вход`);
-    alert(`Вам запрещён вход`);
-   
-        
+    console.log([`resultAge<18`],`Вам запрещён вход`);
+    alert(`Вам запрещён вход`);  
 }
-else if(resultAge>60){
-    console.log(`Дышите глубже, у вас всё получиться`);
+else if(resultAge>70){
+    console.log([`resultAge>60`],`Дышите глубже, у вас всё получиться`);
     alert(`Дышите глубже, у вас всё получиться`);
-    
-}
-console.log(`##############################################################`);
-const confQuestion=confirm(`Любите ли вы деньги?`);
+    console.log(`##############################################################`);
+}else if(resultAge>18 || resultAge<=69){
+    const confQuestion=confirm(`Любите ли вы деньги?`);
 if(confQuestion===true){
     console.log([`confQuestion`], confQuestion);
-    alert(`Добро пожаловать на семинар`+firstName+lastName+`!`);
-
+    alert(`Многоуважаемый`+` `+firstName+` `+lastName+`,`+` `+` `+`добро пожаловать на семинар`+`!`);
 }
 else{
     alert(`Извините, желаем вам удачи!`)
 }
+}    
+
+
+
+
+
+
+
+// console.log(`##############################################################`);
+// const confQuestion=confirm(`Любите ли вы деньги?`);
+// if(confQuestion===true){
+//     console.log([`confQuestion`], confQuestion);
+//     alert(`Многоуважаемый`+` `+firstName+` `+lastName+`,`+` `+` `+`добро пожаловать на семинар`+`!`);
+
+// }
+// else{
+//     alert(`Извините, желаем вам удачи!`)
+// }
+
