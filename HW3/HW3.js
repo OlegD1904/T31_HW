006c194a507be61be46917e62066c54c37661982
Dundiev Oleh <dundev1904@gmail.com>
2023-08-25 17:43:55 +0200

    HW3 задание 2 finish

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -19,7 +19,6 @@ const rows=prompt( `Введи количество столбцов:`);
 
 function bildPiramid(cols, rows){
     const sharp=`#`;
-    // const space=` `;
     const breakPoint =`\n`;
     let target=` `;
     for(let i = 0 ; i<cols; i++){
@@ -38,4 +37,27 @@ console.log(`*************************************************`)
 console.log(` `+`#`+` `+`#`+` `+`#`);
 console.log(`#`+` `+`#`+` `+`#`+` `+`#`);
 console.log(` `+`#`+` `+`#`+` `+`#`);
-console.log(`#`+` `+`#`+` `+`#`+` `+`#`);
+console.log(`*************************************************`)
+
+const cols1=prompt( `Введи количество колонок:`);
+const rows1=prompt( `Введи количество столбцов:`);
+function bildPiramid1(rows, cols) {
+    let target1 = "";
+
+    for (let i = 0; i < rows1; i++) {
+        for (let j = 0; j < cols1; j++) {
+            if ((i + j) % 2 === 0) {
+                target1 += "#";
+            } else {
+                target1 += " ";
+            }
+        }
+        target1 += "\n";
+    }
+
+    return target1;
+}
+
+
+const target1 = bildPiramid1(rows, cols);
+console.log(target1);
