f19a627a0be32b0b47a0af118fb8ea477256cc29
Oleh <dundev1904@gmail.com>
2023-08-30 12:18:32 +0200

    HW4 start

---
+++ HW4/HW4.js
@@ -0,0 +1,26 @@
+`use strict`
+// ! Homework :
+// 1) Create convertor from $ to Euro -> get from user sum ($), 
+// then convert this sum to Euro
+// * You might give to user various of covert : From Euro to $ , or $ to Error 
+
+// 2) Create empty object (define it as variable me)
+// via prompts fill such fields as : name ,age , surname 
+// !* Cycles
+
+// 3) With objects created before make next actions : 
+// 1. Delete one field of the keys (ur pick)
+// 2. Update value one of values (ur pick)
+
+
+// function returnString(str){
+//     return [str];
+// }
+
+// const returnedStr = returnString("Message");
+// returnedStr.forEach(el => {
+//     el.
+// })
+
+// returnString.
+console.log(`mmcxm`)
