385741543864b4c02d4dbd1a0fb3c16564f812a5
Dundiev Oleh <dundev1904@gmail.com>
2023-09-06 16:34:28 +0200

    HW 4 10_07_23 start

---
+++ HW 10.07.23/10_07_23.js
@@ -0,0 +1,42 @@
+`use strict`
+console.log(`lsklös`)
+// HOMEWORK
+
+// 1) Create programm wich will recieve information about user : name , email , password , 
+     //Создайте программу, которая будет получать информацию о пользователе: имя, адрес электронной почты, пароль,
+// * skills (amount : uncountable) (per one prompt) ->
+   // * навыки (количество: неисчисляемое) (за одно приглашение) ->
+// * skills in the end of interview should look as an arr 
+    // * навыки в конце собеседования должны выглядеть как приглашение
+// ** if amount of skills less than 3 print -> "Useless pice of shit"
+    // ** если количество навыков меньше 3, напечатайте -> «Бесполезный кусок дерьма»
+// ** if amount of skills bigger than 5 print and less than 8 -> "Cool"
+    // ** если количество навыков больше 5, выведите меньше 8 -> "Круто"
+// ** if something else -> "WoW!"
+    // ** если что-то другое -> "Вау!"
+
+// 2) Email should contain following symbols : . , @
+    // 2) Электронное письмо должно содержать следующие символы: . , @
+// ! fullstop symbol should be before the sanilSymbol
+    // ! символ полной остановки должен быть перед sanilSymbol
+
+// 3) Password must cotain : numbers + strings 
+    // 3) Пароль должен содержать: цифры + строки
+// length > 8
+    // длина > 8
+
+// We should only warning user about amoun of symbols , but in it doesn't concern 
+    // Мы должны предупреждать пользователя только о количестве символов, но это не имеет значения
+// our User it might keep "stupid password"
+    // наш пользователь может хранить «глупый пароль»
+    
+// 4) Login .
+    // 4) Войти.
+// all data above keep in arr , so if user registred u should propose 
+    // все данные выше сохраняются в arr, поэтому, если пользователь зарегистрирован, вам следует предложить
+// user login , if email is correct - user should fill password field 
+    // логин пользователя, если адрес электронной почты верен - пользователь должен заполнить поле пароля
+// if  email and password are correct -> congratulations 
+    // если адрес электронной почты и пароль верны -> поздравляем
+// Otherwise show a message with Error on the screen
+    // В противном случае покажем сообщение об ошибке на экране
